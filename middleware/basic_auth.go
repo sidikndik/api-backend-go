9f334a61d4c47f358ca37b28c8faae09b2acfcec
sidikndik <sidikndik92gmail.com>
2024-05-10 04:18:15 +0700

    add basic auth

---
+++ middleware/basic_auth.go
@@ -0,0 +1,32 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+)
+
+var(
+	USERNAME = "sidikndik"
+	PASSWORD = "12345"
+)
+
+func MiddlewareBacicAuth(next http.Handler) http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		
+		log.Print("Executing MiddlewareBacicAuth")
+		
+		username, password, ok := r.BasicAuth()
+		if !ok {
+            w.Write([]byte(`something went wrong`))
+            return
+        }
+
+        isValid := (username == USERNAME) && (password == PASSWORD)
+        if !isValid {
+            w.Write([]byte(`wrong username/password`))
+            return
+        }
+		
+		next.ServeHTTP(w,r)
+	})
+}
